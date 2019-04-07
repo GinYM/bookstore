7ace7e941566d5f2582e232d345cecdd58bb14e4
GinYM <jinyiming1996@gmail.com>
2019-04-07 12:12:46 -0700

    scrap from amazon bookstore

--- scrapper/src/main/java/com/bookstore/scraper/Service/impl/BookScraperImpl.java
+++ scrapper/src/main/java/com/bookstore/scraper/Service/impl/BookScraperImpl.java
@@ -5,6 +5,9 @@ import com.bookstore.scraper.Enum.ResultEnum;
 import com.bookstore.scraper.Exception.ScrapException;
 import com.bookstore.scraper.Service.BookScraper;
 import com.bookstore.scraper.Util.TransformUtil;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import lombok.extern.slf4j.Slf4j;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -24,13 +27,18 @@ public class BookScraperImpl implements BookScraper {
         BookDTO bookDTO = new BookDTO();
         bookDTO.setUrl(url);
         try{
-            Document doc = Jsoup.connect(url).get();
+            Document doc = Jsoup.connect(url)
+                    .userAgent("Mozilla/5.0 (Windows; U; WindowsNT 5.1; en-US; rv1.8.1.6) Gecko/20070725 Firefox/2.0.0.6")
+                    .referrer("http://www.google.com")
+                    .get();
 
             if(doc == null){
                 throw new ScrapException(ResultEnum.DOC_EMPTY);
             }
 
-            Element originalPrice = doc.select("div.price-original").first();
+            //System.out.println(doc.body());
+
+            Element originalPrice = doc.select("span.a-text-strike").first();
 
             if(originalPrice == null){
                 bookDTO.setListPrice(-1);
@@ -38,60 +46,68 @@ public class BookScraperImpl implements BookScraper {
                 bookDTO.setListPrice(TransformUtil.extractDouble(originalPrice.text()) );
             }
 
-            Element offerPrice = doc.select("div.price-sale").first();
+            Element offerPrice = doc.select("span.offer-price").first();
 
             if(offerPrice == null){
                 throw new ScrapException(ResultEnum.Empty);
             }
 
-            // language set to english
-            bookDTO.setLanguage("english");
-            bookDTO.setCategory("Engineering");
-
             bookDTO.setOurPrice(TransformUtil.extractDouble(offerPrice.text()));
+
+            // set category
+            bookDTO.setCategory("Engineering");
 
 
             //System.out.println("size: " +doc.select("li:contains(Author:)").size());
 
-            Element author = doc.select("li:contains(Author:)").first();
+            Element author = doc.select("span.author > span.a-declarative > a.a-link-normal").first();
             bookDTO.setAuthor(TransformUtil.removeStr("Author:", author.text()));
 
             Element publisher = doc.select("li:contains(Publisher:)").first();
-            bookDTO.setPublisher(TransformUtil.removeStr("Publisher:", publisher.text()));
+            bookDTO.setPublisher(TransformUtil.removeStr("Publisher:", publisher.text()).replaceAll("\\(.*\\)",""));
 
             Element isbn = doc.selectFirst("li:contains(ISBN-10:)");
             bookDTO.setIsbn(TransformUtil.removeStr("ISBN-10:", isbn.text()));
 
-            Element bookFormat = doc.selectFirst("li:contains(Format:)");
-            bookDTO.setFormat(TransformUtil.removeStr("Format:", bookFormat.text()).toLowerCase());
+            Element language = doc.selectFirst("li:contains(Language:)");
+            bookDTO.setLanguage(TransformUtil.removeStr("Language:", language.text()).toLowerCase());
 
-            Element weight = doc.selectFirst("li:contains(Weight:)");
+            Elements bookHeaderInfo = doc.select("h1#title > span");
+
+            Element bookFormat = bookHeaderInfo.get(1);
+            bookDTO.setFormat(bookFormat.text().toLowerCase());
+
+            Element publishDate = bookHeaderInfo.get(2);
+            String extractDate = publishDate.text().substring(2);
+            bookDTO.setPublicationDate(TransformUtil.extractDate(extractDate));
+
+            Element pages = doc.selectFirst("li:contains(Hardcover:)");
+            bookDTO.setNumberOfPages(Integer.parseInt(TransformUtil.removeStr("Hardcover:", pages.text()).split(" ")[0]));
+
+            Element weight = doc.selectFirst("li:contains(Shipping Weight:)");
             if(weight==null){
                 bookDTO.setShippingWeight(-1);
             }else{
-                bookDTO.setShippingWeight(Double.parseDouble(TransformUtil.removeStr("Weight:", weight.text())));
+                bookDTO.setShippingWeight(Double.parseDouble(TransformUtil.removeStr("Shipping Weight:", weight.text()).split(" ")[0]));
             }
 
-            Element title = doc.selectFirst("li:contains(Subject:)");
-            if(title!=null){
-                bookDTO.setTitle(TransformUtil.removeStr("Subject:", title.text()));
-            }else{
-                title = doc.selectFirst("h1.product-detail__name");
-                if(title!=null){
-                    bookDTO.setTitle(title.text());
-                }
-            }
+            Element title = doc.selectFirst("span#productTitle");
+            bookDTO.setTitle(title.text());
 
 
-            Element desc = doc.selectFirst("[name='abstract']");
-            bookDTO.setDescription(desc.attr("content"));
+            Element desc = doc.selectFirst("div#bookDescription_feature_div > noscript");
+            bookDTO.setDescription(desc.text());
 
-            Element inStock = doc.selectFirst("div.skus__quantity");
-            bookDTO.setInStockNumber(TransformUtil.extractInteger(inStock.text()));
+            Element inStock = doc.selectFirst("div#turboState > script");
+            //Element inStock = doc.selectFirst("div.a-button-stack > div#buyNow");
+            //System.out.println(inStock.html());
+            JsonObject obj = new JsonParser().parse(inStock.html()).getAsJsonObject();
+            //System.out.println(obj);
+            JsonElement inStockNumber = obj.getAsJsonObject("eligibility").get("stockOnHand");
+            bookDTO.setInStockNumber(inStockNumber.getAsInt());
 
-            Element publishDate = doc.selectFirst("li:contains(Published:)");
-            String extractDate = TransformUtil.removeStr("Published:", publishDate.text());
-            bookDTO.setPublicationDate(TransformUtil.extractDate(extractDate));
+
+
 
         }catch (Exception e){
             e.printStackTrace();

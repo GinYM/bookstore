df936e3c26a5050cc30b1669033b07b1da200a57
GinYM <jinyiming1996@gmail.com>
2019-04-17 15:15:20 -0700

    use json TextMessage to transform data

--- admin/src/main/java/com/bookstore/service/impl/QueueConsumer.java
+++ admin/src/main/java/com/bookstore/service/impl/QueueConsumer.java
@@ -4,6 +4,9 @@ package com.bookstore.service.impl;
 import com.bookstore.DTO.BookDTO;
 //import com.bookstore.domain.Book;
 import com.bookstore.service.BookService;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 //import org.springframework.amqp.rabbit.annotation.RabbitListener;
@@ -14,6 +17,7 @@ import org.springframework.stereotype.Component;
 import javax.jms.Message;
 import javax.jms.MessageListener;
 import javax.jms.ObjectMessage;
+import javax.jms.TextMessage;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -31,9 +35,12 @@ public class QueueConsumer{
 
     @JmsListener(destination = "${queue.name}")
     public void receiveMessage(final Message message) {
+        ObjectMapper objectMapper = new ObjectMapper();
         try{
-            ObjectMessage objectMessage = (ObjectMessage) message;
-            BookDTO bookDTO = (BookDTO)objectMessage.getObject();
+            //ObjectMessage objectMessage = (ObjectMessage) message;
+            TextMessage textMessage = (TextMessage) message;
+            //BookDTO bookDTO = (BookDTO)objectMessage.getObject();
+            BookDTO bookDTO = objectMapper.readValue(textMessage.getText(), BookDTO.class);
             if(bookDTO!=null){
                 log.info("receive book: {}", bookDTO);
                 bookService.saveBookDTO(bookDTO);
@@ -51,10 +58,17 @@ public class QueueConsumer{
 
     @JmsListener(destination = "${queue.name.all}")
     public void receiveMessageAll(final Message message) {
+        ObjectMapper objectMapper = new ObjectMapper();
 
         try{
-            ObjectMessage objectMessage = (ObjectMessage) message;
-            List<BookDTO> bookDTOList = (List<BookDTO>)objectMessage.getObject();
+            //ObjectMessage objectMessage = (ObjectMessage) message;
+            TextMessage textMessage = (TextMessage) message;
+            //List<BookDTO> bookDTOList = (List<BookDTO>)message.getBody(List.class);
+            //ArrayList<BookDTO> bookDTOList = (ArrayList<BookDTO>)objectMessage.getObject();
+
+            List<BookDTO> bookDTOList = objectMapper.readValue(textMessage.getText(),
+                    new TypeReference<List<BookDTO>>(){});
+
             if(bookDTOList!=null){
                 log.info("receive book number: {}", bookDTOList.size());
                 this.bookDTOList = bookDTOList;

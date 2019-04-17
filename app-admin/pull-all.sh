8ded1e19a825f996a38cac215d4dcae9b5971b1e
GinYM <jinyiming1996@gmail.com>
2019-04-17 15:59:41 -0700

    add docker-compose

---
+++ app-admin/pull-all.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+docker pull 030355243416.dkr.ecr.us-east-1.amazonaws.com/bookstore/scrapper:latest
+docker pull 030355243416.dkr.ecr.us-east-1.amazonaws.com/bookstore/adminportal:latest
